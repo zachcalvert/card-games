db664d7539b847bacd1501d3f29c987c95c16821
Zach Calvert <zcalvert@oreilly.com>
2020-06-27 19:59:24 -0700

    Correct key in player pass logic

--- web/cribbage/__init__.py
+++ web/cribbage/__init__.py
@@ -167,8 +167,8 @@ def peg_round_action(msg):
             return
 
     else:
-        bev.record_pass(msg['game'], msg['nickname'])
-        emit('show_player_passed', {'player': msg['nickname']}, room=msg['game'])
+        bev.record_pass(msg['game'], msg['player'])
+        emit('show_player_passed', {'player': msg['player']}, room=msg['game'])
 
     next_player, go_point_wins = bev.next_player(msg['game'])
     if go_point_wins:
